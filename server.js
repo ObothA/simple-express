01fb856b2de0901295d23f98eedf42985f19f4b2
obotha <khyper234@gmail.com>
2021-05-11 20:56:07 +0300

    Add a routes file for persons.

--- server.js
+++ server.js
@@ -1,7 +1,48 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+// eslint-disable-next-line no-unused-vars
 const colors = require('colors');
 
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect ot database
+connectDB();
+
+/** Route files */
+const persons = require('./routes/persons');
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+/** Mount Routers */
+app.use('/api/v1/persons', persons);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+// eslint-disable-next-line no-unused-vars
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
